51d81f14e56c9c2e506d26cbba9501315dc14a57
Gerardo Cedillo <yousagger@gmail.com>
2020-06-29 16:12:55 -0500

    Estructura clase

---
+++ 2. Práctica/Definición_clase.py
@@ -0,0 +1,9 @@
+# Definición de clase
+
+class <nombre_de_la_clase>(<super_clase>):
+
+    def __init__(self, <params>):
+        <expresion>
+        
+    def <nombre_del_metodo>(self, <params>):
+        <expresion>
