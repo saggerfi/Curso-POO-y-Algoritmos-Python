358da489001a6b7c28b33725e9c4fcd9b69c2fa8
Gerardo Cedillo <yousagger@gmail.com>
2020-07-06 19:19:53 -0500

    Contador de iteraciones incluido

--- 9. Busqueda binaria/Busqueda_binaria.py
+++ 9. Busqueda binaria/Busqueda_binaria.py
@@ -1,18 +1,18 @@
 import random
 
-def busqueda_binaria(lista, comienzo, final, objetivo):
-    print(f'buscando {objetivo} entre {lista[comienzo]} y {lista[final - 1]} ')
+def busqueda_binaria(lista, comienzo, final, objetivo, i = 0):
+    i += 1
     if comienzo > final:
-        return False
+        return (False, i)
 
     medio = (comienzo + final) // 2
 
     if lista[medio] == objetivo:
-        return True
+        return (True, i)
     elif lista [medio] < objetivo:
-        return busqueda_binaria(lista, medio + 1, final, objetivo)
+        return busqueda_binaria(lista, medio + 1, final, objetivo, i = i)
     else:
-        return busqueda_binaria(lista, comienzo, medio - 1, objetivo)
+        return busqueda_binaria(lista, comienzo, medio - 1, objetivo, i = i)
 
 
 
@@ -21,8 +21,9 @@ if __name__ == '__main__':
     objetivo = int(input('¿Qué número quieres encontrar? '))
 
     lista = sorted([random.randint(0,100) for i in range(tamaño_de_lista)])
-
-    encontrado = busqueda_binaria(lista, 0, len(lista), objetivo)
+    
+    (encontrado, i) = busqueda_binaria(lista, 0, len(lista), objetivo)
 
     print(lista)
     print(f'El elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
+    print(f'Iteraciones busqueda binaria: {i}')
