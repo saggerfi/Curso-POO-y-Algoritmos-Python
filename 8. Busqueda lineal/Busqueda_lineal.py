c4ab81f882e53501f90670c79e4ba02679daa2f1
Gerardo Cedillo <yousagger@gmail.com>
2020-07-06 19:13:53 -0500

    Incluimos contador

--- 8. Busqueda lineal/Busqueda_lineal.py
+++ 8. Busqueda lineal/Busqueda_lineal.py
@@ -1,14 +1,15 @@
 import random
 
-def busqueda_lineal(lista, objetivo):
+def busqueda_lineal(lista, objetivo, i = 0):
     match = False
 
     for elemento in lista: #O(n)
+        i += 1
         if elemento == objetivo:
             match == True
             break
-    
-    return match
+
+    return(match, i)
 
 if __name__ == '__main__':
     tamaño_de_lista = int(input('¿De qué tamaño sera la lista? '))
@@ -16,6 +17,9 @@ if __name__ == '__main__':
 
     lista = [random.randint(0, 100) for i in range(tamaño_de_lista)]
 
+    (encontrado, i) = busqueda_lineal(lista, objetivo)
+
     encontrado = busqueda_lineal(lista, objetivo)
     print(lista)
     print(f'El elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
+    print(f'Número de iteraciones: {i}')
