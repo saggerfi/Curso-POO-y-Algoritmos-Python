8a6ca775b0067490bfb527acd79bfd3fda186291
Gerardo Cedillo <yousagger@gmail.com>
2020-07-12 14:04:25 -0500

    Problema del morral completo

--- 14. El problema del morral/morral.py
+++ 14. El problema del morral/morral.py
@@ -5,7 +5,7 @@ def morral(tamaño_morral, pesos, valores, n):
     if pesos[n - 1] > tamaño_morral:
         return morral(tamaño_morral, pesos, valores, n - 1)
     
-    if 
+    return max(valores[n - 1] + morral(tamaño_morral - pesos[n - 1], pesos, valores, n - 1), morral(tamaño_morral, pesos, valores, n - 1))
 
 if __name__ == '__main__':
     valores = [60, 100 , 120]
